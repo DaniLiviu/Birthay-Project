cf854baa8a3cc0ab713491661af55dc1a4105304
ardelivi <ardelivi@amazon.com>
2024-02-11 21:29:34 +0000

    changing the code to make it more tidy

--- Personalized Birthday Wishes.py
+++ Personalized Birthday Wishes.py
@@ -13,19 +13,13 @@ print('Hello', user_name, "Let's celebrate your", user_age, "years of awesomenes
 print()
 print('Wishing you a day filled with joy and laughter as you turn', user_age,'year old!!')
 print()
-print ("Just like a fine wine, you seem to get better with age. Happy birthday, handsome guy!")
+print ("Just like a fine wine, you seem to get better with age. Happy birthday!")
 print()
 print()
 print("With love and best wishes,")
 print("Daniel.")
-#print("Your age is {}".format (age))
-#print("In one year you will be {} years old".format(age +1))
 
-#print ("What is you date of birth ?") 
-#user_age = int(input("What is your date of Birth ? : "))
-#a = (int(2024))
-#b = (int(birth))
-#print (sum (a) - (b))
+
 
 
 
