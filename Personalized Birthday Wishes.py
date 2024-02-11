7163efffadf02bd13b6a5fed80bb48cff2199e29
ardelivi <ardelivi@amazon.com>
2024-02-11 18:30:08 +0000

    adding birthay file in to repository

---
+++ Personalized Birthday Wishes.py
@@ -0,0 +1,34 @@
+#Asking what is your Name
+print("Hi what's your name?")
+user_name = input()
+#Aks what year where you bonn in.
+print("Hello {} What year where you bron in (ex:1900)? ".format(user_name))
+age = int(input())
+a = 2024
+b = age
+user_age = int((a) - (b))
+print()
+print()
+print('Hello', user_name, "Let's celebrate your", user_age, "years of awesomeness!")
+print()
+print('Wishing you a day filled with joy and laughter as you turn', user_age,'year old!!')
+print()
+print ("Just like a fine wine, you seem to get better with age. Happy birthday, handsome guy!")
+print()
+print()
+print("With love and best wishes,")
+print("Daniel.")
+#print("Your age is {}".format (age))
+#print("In one year you will be {} years old".format(age +1))
+
+#print ("What is you date of birth ?") 
+#user_age = int(input("What is your date of Birth ? : "))
+#a = (int(2024))
+#b = (int(birth))
+#print (sum (a) - (b))
+
+
+
+
+
+
